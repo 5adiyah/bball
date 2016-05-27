79060a62124fde67aec988daa3f7d1662c7f61e5
sadiyah <sadiyah-ali@hotmail.com>
2016-05-26 20:24:48 -0700

    Add paths

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create();
+
+
+/********************          ********************/
+/******************** MY PATHS ********************/
+/********************          ********************/
+
+
+var paths = {
+    jsFiles: './app/js/*.js',
+    jsDirectory: './app/js/',
+    cssFiles: './app/css/*.css',
+    cssDirectory: './app/css/',
+    distributionDirectory: './dist'
+};
 
 
 gulp.task('sassCompiler', function(){
@@ -7,3 +22,11 @@ gulp.task('sassCompiler', function(){
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('app/css'))
 });
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
